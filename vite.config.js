a1937257ddddd8d6fca74a769c31d49f959a36a3
alestagli919 <112497547+alestagli919@users.noreply.github.com>
2026-05-24 14:13:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import path from 'path' // <-- Añadimos esto para manejar las rutas
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  logLevel: 'error', 
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  base: '/aji-web/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // <-- ¡Esto le enseña a Vite qué significa el "@"!
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
